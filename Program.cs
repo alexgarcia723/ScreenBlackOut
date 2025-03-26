6565901eca6d05fff5e1456a163d6f64c8ef2968
alexander garcia <alexthekidmg@gmail.com>
2025-03-26 12:48:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using System.Windows.Forms; // UI components like NotifyIcon
+using System.Drawing;       // Required for Icon, Color, etc.
+
+
+namespace ScreenBlackOut
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
